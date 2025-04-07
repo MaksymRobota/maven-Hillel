da2ac389581ab30a01a0c5464f05889b0e3cebf4
robotam <robotam@fuib.com>
2025-04-07 21:13:04 +0300

    add g

--- src/main/java/com/example/demo/controller/OrdersController.java
+++ src/main/java/com/example/demo/controller/OrdersController.java
@@ -6,7 +6,6 @@ import com.example.demo.service.OrderService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -27,32 +26,32 @@ public class OrdersController {
     OrderService orderService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<OrderDto> getOrderById(@PathVariable Long id) {
-        return ResponseEntity.ok(orderService.getOrderById(id));
+    public OrderDto getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
     }
 
     @PostMapping
-    public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto) {
-        return ResponseEntity.ok(orderService.createOrder(orderDto));
+    public OrderDto createOrder(@RequestBody OrderDto orderDto) {
+        return orderService.createOrder(orderDto);
     }
 
     @GetMapping
-    public ResponseEntity<List<OrderDto>> getAllOrders() {
-        return ResponseEntity.ok(orderService.getAllOrders());
+    public List<OrderDto> getAllOrders() {
+        return orderService.getAllOrders();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<OrderDto> updateOrder(@PathVariable Long id, @RequestBody OrderDto order) {
-        return ResponseEntity.ok(orderService.updateOrder(id, order));
+    public OrderDto updateOrder(@PathVariable Long id, @RequestBody OrderDto order) {
+        return orderService.updateOrder(id, order);
     }
 
     @PatchMapping("/{orderId}/products")
-    public ResponseEntity<OrderDto> addProductToOrder(@PathVariable Long orderId, @RequestBody ProductDto product) {
-        return ResponseEntity.ok(orderService.addProductToOrder(orderId, product));
+    public OrderDto addProductToOrder(@PathVariable Long orderId, @RequestBody ProductDto product) {
+        return orderService.addProductToOrder(orderId, product);
     }
 
     @DeleteMapping("/{orderId}/products/{productId}")
-    public ResponseEntity<OrderDto> removeProductFromOrder(@PathVariable Long orderId, @PathVariable Long productId) {
-        return ResponseEntity.ok(orderService.removeProductFromOrder(orderId, productId));
+    public OrderDto removeProductFromOrder(@PathVariable Long orderId, @PathVariable Long productId) {
+        return orderService.removeProductFromOrder(orderId, productId);
     }
 }
